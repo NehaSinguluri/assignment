c6096c432bd963e3794f0b3b54648464ff0e3377
NehaSinguluri <66587084+NehaSinguluri@users.noreply.github.com>
2020-06-11 12:08:47 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+numbers=['1','2','3','4']
+alphabets=['A','B','C','D']
+colours=('black','red','blue','white','yellow')
+dictionary={'apple':1,'mango':2,'orange':3,'banana':4}
+
+print(numbers)
+print(alphabets)
+alphabets.append('E')
+print(alphabets)
+print(colours[:2])
+del dictionary['banana']
+print(dictionary)
+
+
+# In[ ]:
+
+
+
+
